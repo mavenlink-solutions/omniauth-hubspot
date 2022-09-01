71ea21ca74cdb288d1139b413d7ff60e9ea78f74
Alan So <aso@mavenlink.com>
2022-08-31 17:32:03 -0700

    Added callback_url; If we don't add callback_url then we cannot create a new service for some reason...

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -3,15 +3,17 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class HubspotOauth2 < OmniAuth::Strategies::OAuth2
-      option :name, "hubspot_oauth2"
-
-      args [:client_id, :client_secret]
+      option :name, 'hubspot_oauth2'
 
       option :client_options, {
-        site: 'https://api.hubapi.com',
-        authorize_url: 'https://app.hubspot.com/oauth/authorize',
-        token_url: 'oauth/v1/token'
+        site:           'https://api.hubapi.com',
+        authorize_url:  'https://app.hubspot.com/oauth/authorize',
+        token_url:      '/oauth/v1/token'
       }
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
